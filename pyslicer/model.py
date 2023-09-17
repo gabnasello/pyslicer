c2cb93dc20632aca0f4fe232a7c81a395d373a25
gabnasello <gabriele.nasello@gmail.com>
2023-09-17 10:48:36 -0400

    create hollow cylinders

--- pyslicer/model.py
+++ pyslicer/model.py
@@ -30,3 +30,48 @@ def decimate_model(modelNode, reductionFactor = 0.8):
     slicer.mrmlScene.RemoveNode(cliNode)
     
     return modelNode_deciamted
+
+def create_hollow_cylinder(height=1, 
+                           radius_inf=0.5, radius_sup=1, space =5, 
+                           direction=(1, 0, 0), 
+                           nameModel='Cylinder', 
+                           color=(230/255, 230/255, 77/255), 
+                           opacity=1):
+
+    from pyvista import CylinderStructured
+    from numpy import linspace
+    
+    cyl_hollow = CylinderStructured(radius=linspace(radius_inf, radius_sup, space), height=height, direction=(0, 0, 1))
+
+    cyl_node = slicer.modules.models.logic().AddModel(cyl_hollow.extract_surface())
+        
+    cyl_node.SetName(nameModel)
+    
+    modelDisplayNode = cyl_node.GetDisplayNode()
+    modelDisplayNode.SetColor(color[0], color[1], color[2])
+    modelDisplayNode.SetOpacity(opacity)
+
+    return cyl_node
+
+def register_model_to_points(inputModel, inputFiducials):
+
+    from vtk import vtkMatrix4x4
+    
+    # Create output transform node
+    transformNode = slicer.vtkMRMLTransformNode()
+
+    # Run module logic with default settings
+    fiducialsModelLogic = slicer.modules.fiducialstomodelregistration.widgetRepresentation().self().logic
+    
+    fiducialsModelLogic.run(inputFiducials, inputModel, transformNode)
+
+    transformMatrix = vtkMatrix4x4()
+    transformNode.GetMatrixTransformToWorld(transformMatrix)
+    transformMatrix.Invert()
+
+    # Create new transform node with the inverted transform matrix
+    transformNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTransformNode")
+    transformNode.SetName('Transform Cylinder Defect')
+    transformNode.SetMatrixTransformToParent(transformMatrix);
+
+    return transformNode
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
 setup(name='pyslicer',
-      version='0.1.2',
+      version='0.1.3',
       py_modules=['pyslicer'],
       )
